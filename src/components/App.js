0a3db93a60d281d3cb04ab3d8c6752f99aa8b194
TabarkAlhawadi <tabarkalhawadi1@gmail.com>
2023-02-07 16:47:25 +0300

    test

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+import Body from './Body'
+import Footer from './Footer'
+import './App.css'
+
+
+function App() {
+    return (
+      <React.Fragment>
+           <Header />
+           <Body />
+           <Footer />
+      </React.Fragment>
+  );
+}
+
+
+export default App
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Footer() {
+    return(
+      <React.Fragment>
+        <footer>
+  <p>Author: Hege Refsnes</p>
+  <a href="mailto:hege@example.com">hege@example.com</a>
+</footer>
+      </React.Fragment>      
+    );
+}
+
+export default Footer;
